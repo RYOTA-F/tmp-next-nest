569f4a87600e48697081e222d3b9348a450769c1
RYOTA-F <ry.fj0304@gmail.com>
2022-02-23 18:42:04 +0900

    [add] Nest: TypeORM Migration

--- nest/src/migration/1645609263314-TestMigration.ts
+++ nest/src/migration/1645609263314-TestMigration.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class TestMigration1645571563482 implements MigrationInterface {
-    name = 'TestMigration1645571563482'
+export class TestMigration1645609263314 implements MigrationInterface {
+    name = 'TestMigration1645609263314'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE \`tests\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, \`score\` int NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
