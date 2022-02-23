93170dd3a5b503c6e593bfb374d2c31ef4b4bcd2
RYOTA-F <ry.fj0304@gmail.com>
2022-02-23 23:47:57 +0900

    [add] Next ESLint

--- nest/src/database/migration/1645618817366-TestsMigration.ts
+++ nest/src/database/migration/1645618817366-TestsMigration.ts
@@ -1,14 +1,15 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm'
 
 export class TestsMigration1645618817366 implements MigrationInterface {
-    name = 'TestsMigration1645618817366'
+  name = 'TestsMigration1645618817366'
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE \`tests\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, \`score\` int NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE \`tests\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, \`score\` int NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`
+    )
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE \`tests\``);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE \`tests\``)
+  }
 }
